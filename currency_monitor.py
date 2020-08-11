43a4dae5f23689280375872804420f66243e8698
Marcin <marcin94@MacBook-Pro-Marcin.local>
2020-08-11 18:05:58 +0200

    Adding write to file static method, and method for cheking if the price exceeded the minvalue of price'

--- currency_monitor.py
+++ currency_monitor.py
@@ -41,34 +41,34 @@ class PriceTracker:
         r = requests.get('https://finance.yahoo.com/quote/EURPLN=X?p=EURPLN%3DX&.tsrc=fin-srch&guce_referrer'
                          '=aHR0cHM6Ly9maW5hbmNlLnlhaG9vLmNvbS8_Z3VjZV9yZWZlcnJlcj1hSFIwY0hNNkx5OTNkM2N1WjI5dloyeGxMbU52YlM4Jmd1Y2VfcmVmZXJyZXJfc2lnPUFRQUFBRG1vS3ROMkF5bzFpTDRpd29Td0Z4Z0NDTVNXU3M0UkNoa2pBcGl2NmxobmxJcWRab0JIWUF6NVJuNHlZdkN1WTRBNEdwVTRfWjBZQ3JNM1RwX2ZMd05rejg0TkVWdksyUzA3LVNmNXdndUJCUjhieG5sZEN4dGRCRmV6eEZfMnNQdEpQeXJ6UzREeV9WRUF4ZXNUMXNLYzlnTm1pSlFCV3R6LVpLX0hvc2p5Jl9ndWNfY29uc2VudF9za2lwPTE1OTcwODc3MTg&guce_referrer_sig=AQAAAKzjjM2--Diw1M3gykrGHjIn9NdqSch_odxmo6xqtgD4pNoanrEQBgPoZ9xkh8HPYFN1_9mpio4Fg2tEGa4GrsK69bHe4yN9LactTwdKEuBxazZPO751TNSeFH_lltkNoN1k7D6I978v1eXB9WaCp0NUgbRZRmbYEdoZmkmQvUq7&_guc_consent_skip=1597087949')
         soup = BeautifulSoup(r.text, 'html.parser')
-        elem = soup.find('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-        sleep(10)
-        return float(elem.text)
+        price_elem = soup.find('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
+        sleep(1)
+        return float(price_elem.text)
+
+    @staticmethod
+    def check_min_value(tracked_price, min_value: float):
+        if tracked_price < 4.399:
+            print(f'Warning! Price dropeed under {min_value} pln {tracked_price}')
+            PriceTracker.make_phone_call()
+        else:
+            print(f"Current price for Euro in PLN is {tracked_price}")
 
 
-class FileGenerator():
-    def write_to_file(self, name):
-        pass
+    @staticmethod
+    def write_to_file():
+        with open(f'{datetime.utcnow().date()}_data.csv', 'w', newline='') as file:
+            fieldnames = ['date', 'value_in_pln']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            while True:
+                price = PriceTracker.track_price()
+                writer.writerow({'date': datetime.utcnow().strftime("%H:%M:%S"), 'value_in_pln': price})
+
+                PriceTracker.check_min_value(tracked_price=price, min_value=4.399)
 
 
 if __name__ == "__main__":
-    tracker = PriceTracker()
+    PriceTracker().write_to_file()
 
-    with open(f'data.csv', 'w', newline='') as file:
-        fieldnames = ['date', 'value_in_pln']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-        writer.writeheader()
-
-        while True:
-            price = tracker.track_price()
-            writer.writerow({'date': datetime.utcnow(), 'value_in_pln': price})
-
-            if price < 4.399:
-                print(f'Warning! Price dropeed under 4.399 pln {price}')
-
-                # var = False
-            else:
-                print(f"Current price for Euro in PLN is {price}")
-            # print('Dialing an emergency call!')
-            # tracker.make_phone_call()
