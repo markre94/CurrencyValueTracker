7c6a6aecb4b038501e80279e3c23383003aa7e9e
Marcin <marcin94@MacBook-Pro-Marcin.local>
2020-08-11 17:40:06 +0200

    Saving data into csv file

--- currency_monitor.py
+++ currency_monitor.py
@@ -42,25 +42,33 @@ class PriceTracker:
                          '=aHR0cHM6Ly9maW5hbmNlLnlhaG9vLmNvbS8_Z3VjZV9yZWZlcnJlcj1hSFIwY0hNNkx5OTNkM2N1WjI5dloyeGxMbU52YlM4Jmd1Y2VfcmVmZXJyZXJfc2lnPUFRQUFBRG1vS3ROMkF5bzFpTDRpd29Td0Z4Z0NDTVNXU3M0UkNoa2pBcGl2NmxobmxJcWRab0JIWUF6NVJuNHlZdkN1WTRBNEdwVTRfWjBZQ3JNM1RwX2ZMd05rejg0TkVWdksyUzA3LVNmNXdndUJCUjhieG5sZEN4dGRCRmV6eEZfMnNQdEpQeXJ6UzREeV9WRUF4ZXNUMXNLYzlnTm1pSlFCV3R6LVpLX0hvc2p5Jl9ndWNfY29uc2VudF9za2lwPTE1OTcwODc3MTg&guce_referrer_sig=AQAAAKzjjM2--Diw1M3gykrGHjIn9NdqSch_odxmo6xqtgD4pNoanrEQBgPoZ9xkh8HPYFN1_9mpio4Fg2tEGa4GrsK69bHe4yN9LactTwdKEuBxazZPO751TNSeFH_lltkNoN1k7D6I978v1eXB9WaCp0NUgbRZRmbYEdoZmkmQvUq7&_guc_consent_skip=1597087949')
         soup = BeautifulSoup(r.text, 'html.parser')
         elem = soup.find('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-        sleep(1)
+        sleep(10)
         return float(elem.text)
+
 
 class FileGenerator():
     def write_to_file(self, name):
-        with open(name +'.csv', 'w') as f:
-            csv.DictWriter()
+        pass
+
 
 if __name__ == "__main__":
     tracker = PriceTracker()
 
-    # var = True
-    # while True:
-    #     price = tracker.track_price()
-    #     if price < 4.399:
-    #         print(f'Warning! Price dropeed under 4.399 pln {price}')
-    #
-    #         var = False
-    #     else:
-    #         print(f"Current price for Euro in PLN is {price}")
-    #     print('Dialing an emergency call!')
-    #     tracker.make_phone_call()
+    with open(f'data.csv', 'w', newline='') as file:
+        fieldnames = ['date', 'value_in_pln']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        while True:
+            price = tracker.track_price()
+            writer.writerow({'date': datetime.utcnow(), 'value_in_pln': price})
+
+            if price < 4.399:
+                print(f'Warning! Price dropeed under 4.399 pln {price}')
+
+                # var = False
+            else:
+                print(f"Current price for Euro in PLN is {price}")
+            # print('Dialing an emergency call!')
+            # tracker.make_phone_call()
