71c348464cffac8816909e98f2cbf0e6b4d5b8ba
Marcin <marcin94@MacBook-Pro-Marcin.local>
2020-08-12 21:28:54 +0200

    Added SMS report, and changeed @check_min_value to call 3 times - and send a warning message after thitd time'

--- currency_monitor.py
+++ currency_monitor.py
@@ -3,11 +3,18 @@ from bs4 import BeautifulSoup
 from time import sleep
 from twilio.rest import Client
 from datetime import datetime
+from datetime import timedelta
+from datetime import date
 import csv
-import schedule
+import statistics
 
 
 class PriceTracker:
+    def __init__(self, min_value: float, track_to: datetime):
+        self.min_value = min_value
+        self.track_to = track_to
+        self.warnings = 0
+
     @staticmethod
     def make_phone_call():
         auth_token = 'f724b54966b1e70ea3571f5e75c8920e'
@@ -40,41 +47,77 @@ class PriceTracker:
     @staticmethod
     def track_price():
         r = requests.get('https://finance.yahoo.com/quote/EURPLN=X?p=EURPLN%3DX&.tsrc=fin-srch&guce_referrer'
-                         '=aHR0cHM6Ly9maW5hbmNlLnlhaG9vLmNvbS8_Z3VjZV9yZWZlcnJlcj1hSFIwY0hNNkx5OTNkM2N1WjI5dloyeGxMbU52YlM4Jmd1Y2VfcmVmZXJyZXJfc2lnPUFRQUFBRG1vS3ROMkF5bzFpTDRpd29Td0Z4Z0NDTVNXU3M0UkNoa2pBcGl2NmxobmxJcWRab0JIWUF6NVJuNHlZdkN1WTRBNEdwVTRfWjBZQ3JNM1RwX2ZMd05rejg0TkVWdksyUzA3LVNmNXdndUJCUjhieG5sZEN4dGRCRmV6eEZfMnNQdEpQeXJ6UzREeV9WRUF4ZXNUMXNLYzlnTm1pSlFCV3R6LVpLX0hvc2p5Jl9ndWNfY29uc2VudF9za2lwPTE1OTcwODc3MTg&guce_referrer_sig=AQAAAKzjjM2--Diw1M3gykrGHjIn9NdqSch_odxmo6xqtgD4pNoanrEQBgPoZ9xkh8HPYFN1_9mpio4Fg2tEGa4GrsK69bHe4yN9LactTwdKEuBxazZPO751TNSeFH_lltkNoN1k7D6I978v1eXB9WaCp0NUgbRZRmbYEdoZmkmQvUq7&_guc_consent_skip=1597087949')
+                         '=aHR0cHM6Ly9maW5hbmNlLnlhaG9vLmNvbS8_Z3VjZV9yZWZlcnJlcj1hSFIwY0hNNkx5OTNkM2N1WjI5d'
+                         'loyeGxMbU52YlM4Jmd1Y2VfcmVmZXJyZXJfc2lnPUFRQUFBRG1vS3ROMkF5bzFpTDRpd29Td0Z4Z0NDTVN'
+                         'XU3M0UkNoa2pBcGl2NmxobmxJcWRab0JIWUF6NVJuNHlZdkN1WTRBNEdwVTRfWjBZQ3JNM1RwX2ZMd05rej'
+                         'g0TkVWdksyUzA3LVNmNXdndUJCUjhieG5sZEN4dGRCRmV6eEZfMnNQdEpQeXJ6UzREeV9WRUF4ZXNUMXNLYz'
+                         'lnTm1pSlFCV3R6LVpLX0hvc2p5Jl9ndWNfY29uc2Vud'
+                         'F9za2lwPTE1OTcwODc3MTg&guce_referrer_sig=AQAAAKzjjM2--Diw1M3gykrGHjIn9NdqSch_odxmo6xqtgD4pNo'
+                         'anrEQBgPoZ9xkh8HPYFN1_9mpio4Fg2tEGa4GrsK69bHe4yN9LactTwdKEuBxazZPO751TNSeFH_lltkNoN1k7D6I978v'
+                         '1eXB9WaCp0NUgbRZRmbYEdoZmkmQvUq7&_guc_consent_skip=1597087949')
+
         soup = BeautifulSoup(r.text, 'html.parser')
         price_elem = soup.find('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-        sleep(1)
+        sleep(10)
         return float(price_elem.text)
 
-    @staticmethod
-    def check_min_value(tracked_price, min_value: float):
-        if tracked_price < min_value:
-            print(f'Warning! Price dropeed under {min_value} pln {tracked_price}')
-            PriceTracker.make_phone_call()
+    def check_min_value(self, tracked_price):
+        if tracked_price < self.min_value and self.warnings <= 2:
+            print(f'Warning! Price dropeed under {self.min_value} pln {tracked_price}')
+            # PriceTracker.make_phone_call()
+            self.warnings += 1
+        elif tracked_price < self.min_value and self.warnings == 3:
+            # PriceTracker.send_a_report(
+            #     f'This is a warning message. Called 3 times already. Price of EUR/PLN dropped under critical value'
+            # f' {self.min_value} pln')
+            print(f'Called 3 times! Price dropeed under {self.min_value} pln {tracked_price}')
         else:
             print(f"Current price for Euro in PLN is {tracked_price}")
 
-    @staticmethod
-    def write_to_file(to_date: datetime):
-        with open(f'{datetime.utcnow().date()}_data.csv', 'w', newline='') as file:
+    def write_to_file(self):
+        name = datetime.today().date()
+        with open(f'{name}.csv', 'w', newline='') as file:
             fieldnames = ['date', 'value_in_pln']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
+            while datetime.today() < self.track_to:
+                value_of_currency = PriceTracker.track_price()
+                writer.writerow({'date': datetime.today().strftime("%H:%M:%S"), 'value_in_pln': value_of_currency})
 
-            while datetime.today() < to_date:
+                self.check_min_value(tracked_price=value_of_currency)
 
-                price = PriceTracker.track_price()
-                writer.writerow({'date': datetime.utcnow().strftime("%H:%M:%S"), 'value_in_pln': price})
+        return self.generate_report(file.name)
 
-                PriceTracker.check_min_value(tracked_price=price, min_value=4.399)
+    def generate_report(self, file_name: str):
+        """Generates a daily report that will be send with SMS message"""
+        with open(f'{file_name}', 'r') as file:
+            csv_reader = csv.DictReader(file)
+            values_of_eur = []
+            under_min_value = 0
+            for value in csv_reader:
+                values_of_eur.append(float(value ['value_in_pln']))
+                if float(value ['value_in_pln']) < self.min_value:
+                    under_min_value += 1
 
+            body_of_report = f'The daily report of the Value Tracker. The mean EUR/PLN price for {datetime.today().date()} was' \
+                             f' {round(statistics.mean(values_of_eur), 3)}. The currency dropped {under_min_value} times under critical value.'
+            return body_of_report
 
     @staticmethod
-    def generate_report():
-        pass
+    def get_tommorows_date():
+        """ Method returns the tommorrows date at noon. The tracking will last till the end of current day"""
+        # y = datetime.today().date() + timedelta(days=1)
+        # min = datetime.min.time(0,0)
+        dt = datetime.combine(date.today() + timedelta(days=1), datetime.min.time())
+        return dt
 
 
 if __name__ == "__main__":
-    PriceTracker().write_to_file(to_date=datetime(2020,8,11,19,13,57))
-    print('Finished with price tracking')
+    while True:
+        bot = PriceTracker(min_value=4.402, track_to=PriceTracker.get_tommorows_date())
 
+        report = bot.write_to_file()
+
+        bot.send_a_report(message=report)
+
+        print('Finished with price tracking for tommorrow. The report has been send.')
